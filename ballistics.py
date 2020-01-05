d6e08cfbe8f01f241688019ba58af6558d344021
SilentAsAShadow <59534808+SilentAsAShadow@users.noreply.github.com>
2020-01-05 17:24:41 -0600

    Update ballistics.py

--- ballistics.py
+++ ballistics.py
@@ -1,5 +1,8 @@
+print('Wait: Importing libraries')
 import numpy as np
 #import scipy as sp
 import sympy as sym
+import matplotlib
 import matplotlib.pyplot as plt
 #import math
+print('Libraries have been imported')
